17ad3f886ea7c26ade0f2825cb2acea2b8b32126
Amit Kumar Mondal <admin@amitinside.com>
2021-09-29 10:52:05 +0200

    Added new extension to bundles

--- in.bytehue.osgifx.console.ui.bundles/src/main/java/in/bytehue/osgifx/console/ui/bundles/BundlesFxUI.java
+++ in.bytehue.osgifx.console.ui.bundles/src/main/java/in/bytehue/osgifx/console/ui/bundles/BundlesFxUI.java
@@ -1,12 +1,12 @@
 package in.bytehue.osgifx.console.ui.bundles;
 
 import java.io.IOException;
+import java.net.URL;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.inject.Named;
 
-import org.eclipse.fx.core.URLUtils;
 import org.eclipse.fx.core.di.LocalInstance;
 import org.eclipse.fx.core.log.Log;
 import org.eclipse.fx.core.log.Logger;
@@ -28,8 +28,8 @@ public class BundlesFxUI {
 
     @PostConstruct
     public void postConstruct(final VBox parent, @LocalInstance final FXMLLoader loader) {
-        loader.setLocation(
-                URLUtils.createUrl("platform:/plugin/in.bytehue.osgifx.console.ui.bundles/fxml/sample.fxml"));
+        final URL fxml = context.getBundle().getResource("/fxml/sample.fxml");
+        loader.setLocation(fxml);
         try {
             final Node n = loader.load();
             parent.getChildren().add(n);
