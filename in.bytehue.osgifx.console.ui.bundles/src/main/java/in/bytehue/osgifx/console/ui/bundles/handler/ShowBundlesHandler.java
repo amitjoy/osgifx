fa5e8052d56aba98925118c4bc8d76972c2fadec
Amit Kumar Mondal <admin@amitinside.com>
2021-09-29 10:52:05 +0200

    Added new extension to bundles

---
+++ in.bytehue.osgifx.console.ui.bundles/src/main/java/in/bytehue/osgifx/console/ui/bundles/handler/ShowBundlesHandler.java
@@ -0,0 +1,12 @@
+package in.bytehue.osgifx.console.ui.bundles.handler;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+
+public final class ShowBundlesHandler {
+
+    @Execute
+    public void execute() {
+        System.out.println("HHHHHHHEEEEELLLLLLOOOOOOO");
+    }
+
+}
