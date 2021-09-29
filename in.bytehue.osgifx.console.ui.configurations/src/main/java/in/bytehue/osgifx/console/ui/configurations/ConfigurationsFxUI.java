8cfbad908cc4a69c564dde0c364e8d1290d7d1aa
Amit Kumar Mondal <admin@amitinside.com>
2021-09-29 10:50:29 +0200

    Added use of BundleContext from DI

--- in.bytehue.osgifx.console.ui.configurations/src/main/java/in/bytehue/osgifx/console/ui/configurations/ConfigurationsFxUI.java
+++ in.bytehue.osgifx.console.ui.configurations/src/main/java/in/bytehue/osgifx/console/ui/configurations/ConfigurationsFxUI.java
@@ -1,14 +1,16 @@
 package in.bytehue.osgifx.console.ui.configurations;
 
 import java.io.IOException;
+import java.net.URL;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
+import javax.inject.Named;
 
-import org.eclipse.fx.core.URLUtils;
 import org.eclipse.fx.core.di.LocalInstance;
 import org.eclipse.fx.core.log.Log;
 import org.eclipse.fx.core.log.Logger;
+import org.osgi.framework.BundleContext;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -16,14 +18,18 @@ import javafx.scene.layout.VBox;
 
 public class ConfigurationsFxUI {
 
-    @Inject
     @Log
+    @Inject
     private Logger logger;
+
+    @Inject
+    @Named("in.bytehue.osgifx.console.ui.configurations")
+    private BundleContext context;
 
     @PostConstruct
     public void postConstruct(final VBox parent, @LocalInstance final FXMLLoader loader) {
-        loader.setLocation(
-                URLUtils.createUrl("platform:/plugin/in.bytehue.osgifx.console.ui.configurations/fxml/sample.fxml"));
+        final URL fxml = context.getBundle().getResource("/fxml/sample.fxml");
+        loader.setLocation(fxml);
         try {
             final Node n = loader.load();
             parent.getChildren().add(n);
