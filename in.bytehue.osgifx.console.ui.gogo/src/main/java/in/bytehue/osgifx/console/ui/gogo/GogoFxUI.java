f234d49efd55c5c4fdb47ce8e99703a1225eeba5
Amit Kumar Mondal <admin@amitinside.com>
2021-09-30 00:04:40 +0200

    Added Dummy Gogo Extension

---
+++ in.bytehue.osgifx.console.ui.gogo/src/main/java/in/bytehue/osgifx/console/ui/gogo/GogoFxUI.java
@@ -0,0 +1,41 @@
+package in.bytehue.osgifx.console.ui.gogo;
+
+import java.io.IOException;
+import java.net.URL;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.eclipse.fx.core.di.LocalInstance;
+import org.eclipse.fx.core.log.Log;
+import org.eclipse.fx.core.log.Logger;
+import org.osgi.framework.BundleContext;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.layout.VBox;
+
+public class GogoFxUI {
+
+    @Log
+    @Inject
+    private Logger logger;
+
+    @Inject
+    @Named("in.bytehue.osgifx.console.ui.gogo")
+    private BundleContext context;
+
+    @PostConstruct
+    public void postConstruct(final VBox parent, @LocalInstance final FXMLLoader loader) {
+        final URL fxml = context.getBundle().getResource("/fxml/sample.fxml");
+        loader.setLocation(fxml);
+        try {
+            final Node n = loader.load();
+            parent.getChildren().add(n);
+        } catch (final IOException e) {
+            logger.error("Failed to load 'PersonForm.fxml'", e);
+        }
+    }
+
+}
