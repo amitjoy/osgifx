7d21151734037f198805bb6b4bdff67a53a97f5b
Amit Kumar Mondal <admin@amitinside.com>
2021-09-30 13:08:48 +0200

    Added Trimbar Styling

---
+++ in.bytehue.osgifx.console.application/src/main/java/in/bytehue/osgifx/console/application/decoration/DecorationController.java
@@ -0,0 +1,34 @@
+package in.bytehue.osgifx.console.application.decoration;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ToolBar;
+import javafx.stage.Stage;
+
+public class DecorationController implements Initializable {
+
+    @FXML
+    private ToolBar decorationArea;
+
+    public DecorationController() {
+        System.err.println("Creating controller ....");
+    }
+
+    @Override
+    public void initialize(final URL location, final ResourceBundle resources) {
+    }
+
+    @FXML
+    public void handleClose(final ActionEvent event) {
+        Platform.exit();
+    }
+
+    Stage getStage() {
+        return (Stage) decorationArea.getScene().getWindow();
+    }
+}
