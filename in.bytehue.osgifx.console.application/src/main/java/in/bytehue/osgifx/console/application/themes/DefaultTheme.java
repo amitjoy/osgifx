62761d59086d2040d115874f9e2aafafcca468f8
Amit Kumar Mondal <admin@amitinside.com>
2021-09-30 07:16:47 +0200

    Updated Theme

--- in.bytehue.osgifx.console.application/src/main/java/in/bytehue/osgifx/console/application/themes/DefaultTheme.java
+++ in.bytehue.osgifx.console.application/src/main/java/in/bytehue/osgifx/console/application/themes/DefaultTheme.java
@@ -6,14 +6,17 @@ import static org.osgi.service.component.annotations.ReferencePolicy.DYNAMIC;
 import org.eclipse.fx.ui.services.theme.Stylesheet;
 import org.eclipse.fx.ui.services.theme.Theme;
 import org.eclipse.fx.ui.theme.AbstractTheme;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 @Component(service = Theme.class)
 public final class DefaultTheme extends AbstractTheme {
 
-    public DefaultTheme() {
-        super("theme.default", "Default Theme", DefaultTheme.class.getClassLoader().getResource("css/default.css"));
+    @Activate
+    public DefaultTheme(final BundleContext context) {
+        super("theme.default", "Default Theme", context.getBundle().getResource("css/default.css"));
     }
 
     @Override
