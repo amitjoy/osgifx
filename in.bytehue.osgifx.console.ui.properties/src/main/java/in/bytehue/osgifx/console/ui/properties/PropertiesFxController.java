b752d509281508cdd53b1df559325e2ddb23f2c4
Amit Kumar Mondal <admin@amitinside.com>
2021-10-07 00:15:04 +0200

    Added Properties View

---
+++ in.bytehue.osgifx.console.ui.properties/src/main/java/in/bytehue/osgifx/console/ui/properties/PropertiesFxController.java
@@ -0,0 +1,47 @@
+package in.bytehue.osgifx.console.ui.properties;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javax.inject.Inject;
+
+import org.controlsfx.control.table.TableFilter;
+
+import in.bytehue.osgifx.console.agent.dto.XPropertyDTO;
+import in.bytehue.osgifx.console.ui.service.DataProvider;
+import in.bytehue.osgifx.console.util.fx.DTOCellValueFactory;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public final class PropertiesFxController implements Initializable {
+
+    @Inject
+    private DataProvider dataProvider;
+
+    @FXML
+    private TableView<XPropertyDTO> propertyTable;
+
+    @FXML
+    private TableColumn<XPropertyDTO, String> propertyName;
+
+    @FXML
+    private TableColumn<XPropertyDTO, String> propertyValue;
+
+    @FXML
+    private TableColumn<XPropertyDTO, String> propertyType;
+
+    @Override
+    public void initialize(final URL location, final ResourceBundle resources) {
+        propertyName.setCellValueFactory(new DTOCellValueFactory<>("name", String.class));
+        propertyValue.setCellValueFactory(new DTOCellValueFactory<>("value", String.class));
+        propertyType.setCellValueFactory(new DTOCellValueFactory<>("type", String.class));
+
+        propertyTable.setItems(FXCollections.observableArrayList(dataProvider.properties()));
+
+        TableFilter.forTableView(propertyTable).apply();
+    }
+
+}
--- in.bytehue.osgifx.console.ui.properties/src/main/java/in/bytehue/osgifx/console/ui/properties/PropertiesFxUI.java
+++ in.bytehue.osgifx.console.ui.properties/src/main/java/in/bytehue/osgifx/console/ui/properties/PropertiesFxUI.java
@@ -1,26 +1,19 @@
 package in.bytehue.osgifx.console.ui.properties;
 
-import java.io.IOException;
-import java.net.URL;
-
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.eclipse.e4.ui.di.Focus;
 import org.eclipse.fx.core.di.LocalInstance;
-import org.eclipse.fx.core.log.Log;
-import org.eclipse.fx.core.log.Logger;
 import org.osgi.framework.BundleContext;
 
+import in.bytehue.osgifx.console.util.fx.Fx;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.layout.VBox;
 
 public final class PropertiesFxUI {
-
-    @Log
-    @Inject
-    private Logger logger;
 
     @Inject
     @Named("in.bytehue.osgifx.console.ui.properties")
@@ -28,14 +21,18 @@ public final class PropertiesFxUI {
 
     @PostConstruct
     public void postConstruct(final VBox parent, @LocalInstance final FXMLLoader loader) {
-        final URL fxml = context.getBundle().getResource("/fxml/tab-content.fxml");
-        loader.setLocation(fxml);
-        try {
-            final Node n = loader.load();
-            parent.getChildren().add(n);
-        } catch (final IOException e) {
-            logger.error("Failed to load 'PersonForm.fxml'", e);
-        }
+        createControls(parent, loader);
+    }
+
+    @Focus
+    public void focus(final VBox parent, @LocalInstance final FXMLLoader loader) {
+        parent.getChildren().clear();
+        createControls(parent, loader);
+    }
+
+    private void createControls(final VBox parent, final FXMLLoader loader) {
+        final Node tabContent = Fx.loadFXML(loader, context, "/fxml/tab-content.fxml");
+        parent.getChildren().add(tabContent);
     }
 
 }
