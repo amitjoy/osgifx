a4cf7e50cea5a69bf6b94cd65363ee381fdadc9e
Amit Kumar Mondal <admin@amitinside.com>
2021-10-07 10:55:21 +0200

    Added functionality to start/stop/uninstall bundle and update UI

---
+++ in.bytehue.osgifx.console.application/src/main/java/in/bytehue/osgifx/console/application/handler/BundleUninstallHandler.java
@@ -0,0 +1,47 @@
+package in.bytehue.osgifx.console.application.handler;
+
+import static in.bytehue.osgifx.console.event.topics.BundleActionEventTopics.BUNDLE_UNINSTALLED_EVENT_TOPIC;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.core.services.events.IEventBroker;
+import org.eclipse.fx.core.log.Log;
+import org.eclipse.fx.core.log.Logger;
+
+import in.bytehue.osgifx.console.agent.ConsoleAgent;
+import in.bytehue.osgifx.console.supervisor.ConsoleSupervisor;
+
+public final class BundleUninstallHandler {
+
+    @Log
+    @Inject
+    private Logger logger;
+
+    @Inject
+    private IEventBroker eventBroker;
+
+    @Inject
+    private ConsoleSupervisor supervisor;
+
+    @Execute
+    public void execute(@Named("id") final String id) {
+        final ConsoleAgent agent = supervisor.getAgent();
+        if (supervisor.getAgent() == null) {
+            logger.error("Remote agent cannot be connected");
+            return;
+        }
+        try {
+            final String error = agent.uninstall(Long.parseLong(id));
+            if (error == null) {
+                eventBroker.send(BUNDLE_UNINSTALLED_EVENT_TOPIC, id);
+            } else {
+                logger.error(error);
+            }
+        } catch (final Exception e) {
+            logger.error("Bundle " + id + "cannot be uninstalled");
+        }
+    }
+
+}
