8274752ec721f9f4ea4478b9542d37df1e2607ec
Amit Kumar Mondal <admin@amitinside.com>
2021-10-08 15:32:10 +0200

    Disabled Table Row Selection

--- in.bytehue.osgifx.console.ui.services/src/main/java/in/bytehue/osgifx/console/ui/services/ServicesFxController.java
+++ in.bytehue.osgifx.console.ui.services/src/main/java/in/bytehue/osgifx/console/ui/services/ServicesFxController.java
@@ -43,6 +43,7 @@ public final class ServicesFxController implements Initializable {
 
     @Override
     public void initialize(final URL location, final ResourceBundle resources) {
+        table.setSelectionModel(null);
         createControls();
     }
 
