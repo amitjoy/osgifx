46ad598e2f55c08c154b88d7d494ef62fbef1d9e
Amit Kumar Mondal <admin@amitinside.com>
2021-10-12 00:53:59 +0200

    Added Object Class to Service View

--- in.bytehue.osgifx.console.ui.services/src/main/java/in/bytehue/osgifx/console/ui/services/ServicesFxController.java
+++ in.bytehue.osgifx.console.ui.services/src/main/java/in/bytehue/osgifx/console/ui/services/ServicesFxController.java
@@ -63,23 +63,23 @@ public final class ServicesFxController implements Initializable {
 
         final TableColumn<XServiceDTO, String> serviceIdColumn = new TableColumn<>("Service ID");
 
-        serviceIdColumn.setPrefWidth(200);
+        serviceIdColumn.setPrefWidth(100);
         serviceIdColumn.setCellValueFactory(new DTOCellValueFactory<>("id", String.class));
+
+        final TableColumn<XServiceDTO, String> objectClassColumn = new TableColumn<>("Object Class");
+
+        objectClassColumn.setPrefWidth(600);
+        objectClassColumn.setCellValueFactory(new DTOCellValueFactory<>("types", String.class));
 
         final TableColumn<XServiceDTO, String> registeringBundleColumn = new TableColumn<>("Registering Bundle");
 
-        registeringBundleColumn.setPrefWidth(450);
+        registeringBundleColumn.setPrefWidth(400);
         registeringBundleColumn.setCellValueFactory(new DTOCellValueFactory<>("registeringBundle", String.class));
-
-        final TableColumn<XServiceDTO, String> registeringBundleIdColumn = new TableColumn<>("Registering Bundle ID");
-
-        registeringBundleIdColumn.setPrefWidth(200);
-        registeringBundleIdColumn.setCellValueFactory(new DTOCellValueFactory<>("bundleId", String.class));
 
         table.getColumns().add(expanderColumn);
         table.getColumns().add(serviceIdColumn);
+        table.getColumns().add(objectClassColumn);
         table.getColumns().add(registeringBundleColumn);
-        table.getColumns().add(registeringBundleIdColumn);
 
         final ObservableList<XServiceDTO> services = dataProvider.services();
         table.setItems(services);
