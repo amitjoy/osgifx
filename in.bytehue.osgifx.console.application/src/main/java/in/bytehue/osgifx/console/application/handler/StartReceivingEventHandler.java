abf59f98ec8d4aea9690e8ed65d8a58779d3616b
Amit Kumar Mondal <admin@amitinside.com>
2021-10-13 02:31:15 +0200

    Added menu items for starting and stopping event receiving mechanism

---
+++ in.bytehue.osgifx.console.application/src/main/java/in/bytehue/osgifx/console/application/handler/StartReceivingEventHandler.java
@@ -0,0 +1,12 @@
+package in.bytehue.osgifx.console.application.handler;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+
+public final class StartReceivingEventHandler {
+
+    @Execute
+    public void execute() {
+
+    }
+
+}
