df8ac3d347eb78f88d3933f2514b328581af4308
Amit Kumar Mondal <admin@amitinside.com>
2021-10-16 08:52:37 +0200

    Added functionality to use bundle version in about app dialog

--- in.bytehue.osgifx.console.application/src/main/java/in/bytehue/osgifx/console/application/fxml/controller/AboutApplicationDialogController.java
+++ in.bytehue.osgifx.console.application/src/main/java/in/bytehue/osgifx/console/application/fxml/controller/AboutApplicationDialogController.java
@@ -1,8 +1,10 @@
 package in.bytehue.osgifx.console.application.fxml.controller;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 
 import org.controlsfx.control.HyperlinkLabel;
+import org.osgi.framework.BundleContext;
 
 import javafx.application.Application;
 import javafx.fxml.FXML;
@@ -10,9 +12,12 @@ import javafx.scene.Node;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
 
 public final class AboutApplicationDialogController {
 
+    @FXML
+    private Text           appName;
     @FXML
     private ImageView      header;
     @FXML
@@ -21,9 +26,13 @@ public final class AboutApplicationDialogController {
     private HyperlinkLabel hyperlink;
     @Inject
     private Application    jfxApplication;
+    @Inject
+    @Named("in.bytehue.osgifx.console.application")
+    private BundleContext  bundleContext;
 
     @FXML
     public void initialize() {
+        appName.setText("OSGi.fx Console (Version " + bundleContext.getBundle().getVersion() + ")");
         hyperlink.setOnAction(event -> {
             final Hyperlink link           = (Hyperlink) event.getSource();
             final String    eclipseWebLink = link.getText();
