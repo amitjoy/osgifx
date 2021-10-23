c7b48fad5c1a94855bc229e231dec48373b038eb
Amit Kumar Mondal <admin@amitinside.com>
2021-10-23 06:47:56 +0200

    Extracted initialization of status bar

--- in.bytehue.osgifx.console.ui.events/src/main/java/in/bytehue/osgifx/console/ui/events/EventsFxUI.java
+++ in.bytehue.osgifx.console.ui.events/src/main/java/in/bytehue/osgifx/console/ui/events/EventsFxUI.java
@@ -3,7 +3,6 @@ package in.bytehue.osgifx.console.ui.events;
 import static in.bytehue.osgifx.console.event.topics.BundleActionEventTopics.BUNDLE_ACTION_EVENT_TOPICS;
 import static in.bytehue.osgifx.console.supervisor.Supervisor.AGENT_CONNECTED_EVENT_TOPIC;
 import static in.bytehue.osgifx.console.supervisor.Supervisor.AGENT_DISCONNECTED_EVENT_TOPIC;
-import static in.bytehue.osgifx.console.supervisor.Supervisor.CONNECTED_AGENT;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
@@ -11,7 +10,6 @@ import javax.inject.Named;
 
 import org.controlsfx.control.MaskerPane;
 import org.controlsfx.control.StatusBar;
-import org.controlsfx.glyphfont.Glyph;
 import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.ui.di.Focus;
 import org.eclipse.e4.ui.di.UIEventTopic;
@@ -23,16 +21,8 @@ import org.osgi.framework.BundleContext;
 import in.bytehue.osgifx.console.util.fx.Fx;
 import javafx.concurrent.Task;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.geometry.Orientation;
 import javafx.scene.Node;
-import javafx.scene.control.Button;
-import javafx.scene.control.Separator;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.paint.Color;
 
 public final class EventsFxUI {
 
@@ -102,34 +92,17 @@ public final class EventsFxUI {
                 super.succeeded();
                 parent.getChildren().clear();
                 parent.setCenter(tabContent);
-                initStatusBar(parent);
+                Fx.initStatusBar(parent, statusBar);
                 progressPane.setVisible(false);
             }
         };
         parent.getChildren().clear();
         parent.setCenter(progressPane);
-        initStatusBar(parent);
+        Fx.initStatusBar(parent, statusBar);
 
         final Thread thread = new Thread(task);
         thread.setDaemon(true);
         thread.start();
-    }
-
-    private void initStatusBar(final BorderPane parent) {
-        final Button button = new Button("", new Glyph("FontAwesome", "DESKTOP"));
-        button.setBackground(new Background(new BackgroundFill(Color.TRANSPARENT, new CornerRadii(2), new Insets(4))));
-        statusBar.getLeftItems().clear();
-        statusBar.getLeftItems().add(button);
-        statusBar.getLeftItems().add(new Separator(Orientation.VERTICAL));
-        final String property = System.getProperty(CONNECTED_AGENT);
-        final String statusBarText;
-        if (property != null) {
-            statusBarText = "Connected to " + property;
-        } else {
-            statusBarText = "Disconnected";
-        }
-        statusBar.setText(statusBarText);
-        parent.setBottom(statusBar);
     }
 
 }
