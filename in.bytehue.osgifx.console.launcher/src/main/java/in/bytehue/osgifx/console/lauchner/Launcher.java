8b911989ac8108ef0b65e35c3f009c9dca1d2982
Amit Kumar Mondal <admin@amitinside.com>
2021-11-20 15:29:46 +0100

    Added logging to application launcher

--- in.bytehue.osgifx.console.launcher/src/main/java/in/bytehue/osgifx/console/lauchner/Launcher.java
+++ in.bytehue.osgifx.console.launcher/src/main/java/in/bytehue/osgifx/console/lauchner/Launcher.java
@@ -4,13 +4,15 @@ import static java.util.Collections.emptyMap;
 import static org.osgi.framework.Constants.SERVICE_PID;
 import static org.osgi.service.component.annotations.ReferenceCardinality.OPTIONAL;
 
+import org.eclipse.fx.core.log.FluentLogger;
+import org.eclipse.fx.core.log.LoggerFactory;
 import org.osgi.service.application.ApplicationDescriptor;
 import org.osgi.service.application.ApplicationException;
 import org.osgi.service.application.ApplicationHandle;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
-import aQute.lib.exceptions.Exceptions;
 import in.bytehue.osgifx.console.propertytypes.MainThread;
 
 @Component
@@ -22,16 +24,29 @@ public final class Launcher implements Runnable {
     @Reference(target = "(" + SERVICE_PID + "=" + APPLICATION_ID + ")", cardinality = OPTIONAL)
     private volatile ApplicationDescriptor applicationDescriptor;
 
+    @Reference
+    private LoggerFactory factory;
+    private FluentLogger  logger;
+
+    @Activate
+    void activate() {
+        logger = FluentLogger.of(factory.createLogger(getClass().getName()));
+    }
+
     @Override
     public void run() {
         try {
-            if (applicationDescriptor != null) {
-                final ApplicationHandle handle = applicationDescriptor.launch(emptyMap());
-                handle.getExitValue(0);
+            if (applicationDescriptor == null) {
+                logger.atError().log("Application descriptor '%s' not found", APPLICATION_ID);
             }
+            logger.atInfo().log("Application descriptor '%s' found", APPLICATION_ID);
+            final ApplicationHandle handle = applicationDescriptor.launch(emptyMap());
+            handle.getExitValue(0);
         } catch (final ApplicationException e) {
-            Exceptions.duck(e);
+            logger.atError().withException(e).log(e.getMessage());
+            throw new RuntimeException(e);
         } catch (final InterruptedException e) {
+            logger.atError().withException(e).log(e.getMessage());
             Thread.currentThread().interrupt();
         }
     }
