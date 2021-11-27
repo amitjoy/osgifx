e716186fc7bbb6c3a81cab1a5ee86a27ebceda02
Amit Kumar Mondal <admin@amitinside.com>
2021-11-27 07:55:05 +0100

    Refactored StartUp Tracker Logs

--- in.bytehue.osgifx.console.application/src/main/java/in/bytehue/osgifx/console/application/FxStartupTracker.java
+++ in.bytehue.osgifx.console.application/src/main/java/in/bytehue/osgifx/console/application/FxStartupTracker.java
@@ -1,5 +1,13 @@
 package in.bytehue.osgifx.console.application;
 
+import static org.eclipse.fx.ui.services.startup.StartupProgressTrackerService.DefaultProgressState.DI_SYSTEM_INITIALIZED;
+import static org.eclipse.fx.ui.services.startup.StartupProgressTrackerService.DefaultProgressState.JAVAFX_INITIALIZED;
+import static org.eclipse.fx.ui.services.startup.StartupProgressTrackerService.DefaultProgressState.JAVAFX_INITIALIZED_LAUNCHER_THREAD;
+import static org.eclipse.fx.ui.services.startup.StartupProgressTrackerService.DefaultProgressState.LOCATION_CHECK_FAILED;
+import static org.eclipse.fx.ui.services.startup.StartupProgressTrackerService.DefaultProgressState.POST_CONTEXT_LF_FINISHED;
+import static org.eclipse.fx.ui.services.startup.StartupProgressTrackerService.DefaultProgressState.WORKBENCH_GUI_SHOWING;
+import static org.eclipse.fx.ui.services.startup.StartupProgressTrackerService.DefaultProgressState.WORKBENCH_GUI_SHOWN;
+
 import org.eclipse.fx.core.app.ApplicationContext;
 import org.eclipse.fx.core.log.FluentLogger;
 import org.eclipse.fx.core.log.LoggerFactory;
@@ -25,7 +33,27 @@ public final class FxStartupTracker implements StartupProgressTrackerService {
 
     @Override
     public void stateReached(final ProgressState state) {
-        logger.atInfo().log("Current state: %s", state.toString());
+        if (state == JAVAFX_INITIALIZED) {
+            logger.atInfo().log("[StartUp] The JavaFX subsystem has been initialized. This state reached on JavaFX event thread");
+        }
+        if (state == JAVAFX_INITIALIZED_LAUNCHER_THREAD) {
+            logger.atInfo().log("[StartUp] The JavaFX subsystem has been initialized. This state reached on launcher thread.");
+        }
+        if (state == DI_SYSTEM_INITIALIZED) {
+            logger.atInfo().log("[StartUp] The DI-System has been initialized");
+        }
+        if (state == POST_CONTEXT_LF_FINISHED) {
+            logger.atInfo().log("[StartUp] The lifecycle has been finished");
+        }
+        if (state == WORKBENCH_GUI_SHOWING) {
+            logger.atInfo().log("[StartUp] The workbench UI is showing");
+        }
+        if (state == WORKBENCH_GUI_SHOWN) {
+            logger.atInfo().log("[StartUp] The workbench UI is shown");
+        }
+        if (state == LOCATION_CHECK_FAILED) {
+            logger.atInfo().log("[StartUp] State reached when check for workspace could not be locked");
+        }
     }
 
 }
