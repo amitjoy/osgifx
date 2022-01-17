0539bd86a089b0d96e9806d19204d7b9f6786652
Amit Kumar Mondal <admin@amitinside.com>
2022-01-17 09:04:13 +0100

    Introduced functionality to use start levels while installing/updating

--- com.osgifx.console.application/src/main/java/com/osgifx/console/application/dialog/BundleInstallDTO.java
+++ com.osgifx.console.application/src/main/java/com/osgifx/console/application/dialog/BundleInstallDTO.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2022 Amit Kumar Mondal
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License.  You may obtain a copy
  * of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
@@ -22,10 +22,12 @@ import org.osgi.dto.DTO;
 public final class BundleInstallDTO extends DTO {
 
     public File    file;
+    public int     startLevel;
     public boolean startBundle;
 
-    public BundleInstallDTO(final File file, final boolean startBundle) {
+    public BundleInstallDTO(final File file, final boolean startBundle, final int startLevel) {
         this.file        = file;
+        this.startLevel  = startLevel;
         this.startBundle = startBundle;
     }
 
