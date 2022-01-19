08ac68e35177293b723f92bfdf93c23f26cc365d
Amit Kumar Mondal <admin@amitinside.com>
2022-01-19 08:56:37 +0100

    Fixed Indentation

--- com.osgifx.console.update.agent/src/main/java/com/osgifx/console/update/agent/UpdateAgentProvider.java
+++ com.osgifx.console.update.agent/src/main/java/com/osgifx/console/update/agent/UpdateAgentProvider.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2022 Amit Kumar Mondal
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License.  You may obtain a copy
  * of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
@@ -410,7 +410,7 @@ public final class UpdateAgentProvider implements UpdateAgent {
         return Stream.of(bundleContext.getBundles())
                      .filter(b -> b.getSymbolicName().equals(bundle.getID().getArtifactId()))
                      .anyMatch(b -> b.getLocation()
-                             .startsWith(BND_LAUNCHER_BUNDLE_LOCATION_PREFIX));
+                     .startsWith(BND_LAUNCHER_BUNDLE_LOCATION_PREFIX));
         // @formatter:on
     }
 
