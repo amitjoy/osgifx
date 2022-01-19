c78105c1adabcb00d335fdd3f60bcd9376be2d82
Amit Kumar Mondal <admin@amitinside.com>
2022-01-19 10:45:53 +0100

    Fixed Issue with Configuration Update

--- com.osgifx.console.ui.configurations/src/main/java/com/osgifx/console/ui/configurations/ConfigurationEditorFxController.java
+++ com.osgifx.console.ui.configurations/src/main/java/com/osgifx/console/ui/configurations/ConfigurationEditorFxController.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2022 Amit Kumar Mondal
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License.  You may obtain a copy
  * of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
@@ -106,6 +106,8 @@ public final class ConfigurationEditorFxController {
             String       effectivePID = null;
             if (pid == null && ocd != null) {
                 effectivePID = ocd.pid;
+            } else {
+                effectivePID = pid;
             }
             if (config.isFactory) {
                 final String ocdFactoryPid = ocd.factoryPid;
