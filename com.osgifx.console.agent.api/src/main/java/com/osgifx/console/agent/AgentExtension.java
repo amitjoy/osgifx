35481be26d347bf963dc8f6afd048ec8867428dc
Amit Kumar Mondal <admin@amitinside.com>
2022-03-09 11:49:56 +0100

    Removed superfluous annotation

--- com.osgifx.console.agent.api/src/main/java/com/osgifx/console/agent/AgentExtension.java
+++ com.osgifx.console.agent.api/src/main/java/com/osgifx/console/agent/AgentExtension.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2022 Amit Kumar Mondal
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License.  You may obtain a copy
  * of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
@@ -17,15 +17,12 @@ package com.osgifx.console.agent;
 
 import java.util.Map;
 
-import org.osgi.annotation.versioning.ConsumerType;
-
 /**
  * Service interface to be used by consumers for providing custom
  * functionalities that can be invoked through the bnd agent.
  * <p>
  * The services must provide the following key as a service property.
  */
-@ConsumerType
 @FunctionalInterface
 public interface AgentExtension {
 
