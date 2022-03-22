006304461e8b6cbc96b76a5a938108d6235c683c
Amit Kumar Mondal <admin@amitinside.com>
2022-03-22 09:15:12 +0100

    Refactored logging

--- com.osgifx.console.logging/src/main/java/com/osgifx/console/logging/FxConsoleLogHandler.java
+++ com.osgifx.console.logging/src/main/java/com/osgifx/console/logging/FxConsoleLogHandler.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2022 Amit Kumar Mondal
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License.  You may obtain a copy
  * of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
@@ -30,44 +30,44 @@ import org.osgi.service.component.annotations.Component;
 @Component(service = Handler.class)
 public final class FxConsoleLogHandler extends Handler {
 
-    private static final String LOG_FILE_NAME = "log.txt";
+	private static final String LOG_FILE_NAME = "log.txt";
 
-    private final FileHandler        fileHandler;
-    private final CustomLogFormatter formatter;
+	private final FileHandler        fileHandler;
+	private final CustomLogFormatter formatter;
 
-    @Activate
-    public FxConsoleLogHandler(final BundleContext context) throws SecurityException, IOException, URISyntaxException {
-        String area = context.getProperty("osgi.instance.area.default");
+	@Activate
+	public FxConsoleLogHandler(final BundleContext context) throws SecurityException, IOException, URISyntaxException {
+		var area = context.getProperty("osgi.instance.area.default");
 
-        // remove the prefix
-        final String prefix = "file:";
-        area = area.substring(area.indexOf(prefix) + prefix.length());
+		// remove the prefix
+		final var prefix = "file:";
+		area = area.substring(area.indexOf(prefix) + prefix.length());
 
-        final String path         = new URI(area).getPath();
-        final File   parent       = new File(path);
-        final File   logDirectory = new File(parent, "log");
+		final var path         = new URI(area).getPath();
+		final var parent       = new File(path);
+		final var logDirectory = new File(parent, "log");
 
-        // create missing directories
-        logDirectory.mkdirs();
+		// create missing directories
+		logDirectory.mkdirs();
 
-        formatter   = new CustomLogFormatter();
-        fileHandler = new FileHandler(logDirectory.getPath() + "/" + LOG_FILE_NAME, true);
-        fileHandler.setFormatter(formatter);
-    }
+		formatter   = new CustomLogFormatter();
+		fileHandler = new FileHandler(logDirectory.getPath() + "/" + LOG_FILE_NAME, true);
+		fileHandler.setFormatter(formatter);
+	}
 
-    @Override
-    public void publish(final LogRecord logRecord) {
-        fileHandler.publish(logRecord);
-    }
+	@Override
+	public void publish(final LogRecord logRecord) {
+		fileHandler.publish(logRecord);
+	}
 
-    @Override
-    public void flush() {
-        fileHandler.flush();
-    }
+	@Override
+	public void flush() {
+		fileHandler.flush();
+	}
 
-    @Override
-    public void close() throws SecurityException {
-        fileHandler.close();
-    }
+	@Override
+	public void close() throws SecurityException {
+		fileHandler.close();
+	}
 
 }
--- com.osgifx.console.logging/src/main/java/com/osgifx/console/logging/UncaughtExceptionHandler.java
+++ com.osgifx.console.logging/src/main/java/com/osgifx/console/logging/UncaughtExceptionHandler.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2022 Amit Kumar Mondal
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License.  You may obtain a copy
  * of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
@@ -25,18 +25,18 @@ import org.osgi.service.component.annotations.Reference;
 @Component
 public final class UncaughtExceptionHandler implements ExceptionHandler {
 
-    @Reference
-    private LoggerFactory factory;
-    private FluentLogger  logger;
+	@Reference
+	private LoggerFactory factory;
+	private FluentLogger  logger;
 
-    @Activate
-    void activate() {
-        logger = FluentLogger.of(factory.createLogger(getClass().getName()));
-    }
+	@Activate
+	void activate() {
+		logger = FluentLogger.of(factory.createLogger(getClass().getName()));
+	}
 
-    @Override
-    public void handleException(final ExceptionData data) {
-        logger.atError().withException(data.throwable()).log("Uncaught Exception");
-    }
+	@Override
+	public void handleException(final ExceptionData data) {
+		logger.atError().withException(data.throwable()).log("Uncaught Exception");
+	}
 
 }
