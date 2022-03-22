6647aa3ee86034d5968f9d6cb003627e10570b11
Amit Kumar Mondal <admin@amitinside.com>
2022-03-22 10:17:11 +0100

    Reformatted Features UI with Java 17

--- com.osgifx.console.ui.feature/src/main/java/com/osgifx/console/ui/feature/handler/FeatureInstallHandler.java
+++ com.osgifx.console.ui.feature/src/main/java/com/osgifx/console/ui/feature/handler/FeatureInstallHandler.java
@@ -22,7 +22,6 @@ import java.net.URL;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map.Entry;
-import java.util.Optional;
 import java.util.Set;
 
 import javax.inject.Inject;
@@ -45,7 +44,6 @@ import com.google.gson.reflect.TypeToken;
 import com.osgifx.console.feature.FeatureDTO;
 import com.osgifx.console.feature.IdDTO;
 import com.osgifx.console.ui.feature.dialog.FeatureInstallDialog;
-import com.osgifx.console.ui.feature.dialog.FeatureInstallDialog.SelectedFeaturesDTO;
 import com.osgifx.console.update.FeatureAgent;
 import com.osgifx.console.util.fx.FxDialog;
 
@@ -54,134 +52,134 @@ import javafx.concurrent.Task;
 @Requirement(effective = "active", namespace = SERVICE_NAMESPACE, filter = "(objectClass=com.osgifx.console.update.FeatureAgent)")
 public final class FeatureInstallHandler {
 
-    @Log
-    @Inject
-    private FluentLogger    logger;
-    @Inject
-    private IEclipseContext context;
-    @Inject
-    private FeatureAgent    featureAgent;
-    @Inject
-    private IWorkbench      workbench;
-    @Inject
-    @Preference(nodePath = "osgi.fx.feature", key = "repos", defaultValue = "")
-    private Value<String>   repos;
-    private ProgressDialog  progressDialog;
+	@Log
+	@Inject
+	private FluentLogger    logger;
+	@Inject
+	private IEclipseContext context;
+	@Inject
+	private FeatureAgent    featureAgent;
+	@Inject
+	private IWorkbench      workbench;
+	@Inject
+	@Preference(nodePath = "osgi.fx.feature", key = "repos", defaultValue = "")
+	private Value<String>   repos;
+	private ProgressDialog  progressDialog;
 
-    @Execute
-    public void execute() {
-        final FeatureInstallDialog dialog = new FeatureInstallDialog();
+	@Execute
+	public void execute() {
+		final var dialog = new FeatureInstallDialog();
 
-        ContextInjectionFactory.inject(dialog, context);
-        logger.atDebug().log("Injected install feature dialog to eclipse context");
-        dialog.init();
+		ContextInjectionFactory.inject(dialog, context);
+		logger.atDebug().log("Injected install feature dialog to eclipse context");
+		dialog.init();
 
-        final Optional<SelectedFeaturesDTO> selectedFeatures = dialog.showAndWait();
-        if (selectedFeatures.isPresent()) {
-            final SelectedFeaturesDTO selected = selectedFeatures.get();
-            udpateOrInstallFeatures(selected.features, selected.archiveURL);
-        }
-    }
+		final var selectedFeatures = dialog.showAndWait();
+		if (selectedFeatures.isPresent()) {
+			final var selected = selectedFeatures.get();
+			updateOrInstallFeatures(selected.features, selected.archiveURL);
+		}
+	}
 
-    private void udpateOrInstallFeatures(final List<? extends Entry<File, FeatureDTO>> features, final String archiveURL) {
-        if (features.isEmpty()) {
-            return;
-        }
-        final Task<Void> task = new Task<>() {
-            final List<FeatureDTO> successfullyInstalledFeatures = Lists.newArrayList();
-            final List<FeatureDTO> notInstalledFeatures          = Lists.newArrayList();
+	private void updateOrInstallFeatures(final List<? extends Entry<File, FeatureDTO>> features, final String archiveURL) {
+		if (features.isEmpty()) {
+			return;
+		}
+		final Task<Void> task = new Task<>() {
+			final List<FeatureDTO> successfullyInstalledFeatures = Lists.newArrayList();
+			final List<FeatureDTO> notInstalledFeatures          = Lists.newArrayList();
 
-            @Override
-            protected Void call() throws Exception {
-                for (final Entry<File, FeatureDTO> feature : features) {
-                    final FeatureDTO f  = feature.getValue();
-                    final String     id = featureIdAsString(f.id);
-                    try {
-                        featureAgent.updateOrInstall(feature.getKey(), archiveURL);
-                        successfullyInstalledFeatures.add(f);
-                        logger.atInfo().log("Feature '%s' has been successfuly installed/updated", id);
-                    } catch (final Exception e) {
-                        notInstalledFeatures.add(f);
-                        logger.atError().withException(e).log("Cannot update or install feature '%s'", id);
-                    }
-                }
-                return null;
-            }
+			@Override
+			protected Void call() throws Exception {
+				for (final Entry<File, FeatureDTO> feature : features) {
+					final var f  = feature.getValue();
+					final var id = featureIdAsString(f.id);
+					try {
+						featureAgent.updateOrInstall(feature.getKey(), archiveURL);
+						successfullyInstalledFeatures.add(f);
+						logger.atInfo().log("Feature '%s' has been successfuly installed/updated", id);
+					} catch (final Exception e) {
+						notInstalledFeatures.add(f);
+						logger.atError().withException(e).log("Cannot update or install feature '%s'", id);
+					}
+				}
+				return null;
+			}
 
-            @Override
-            protected void succeeded() {
-                progressDialog.close();
-                final String header = "Feature Installation";
+			@Override
+			protected void succeeded() {
+				progressDialog.close();
+				final var header = "Feature Installation";
 
-                final StringBuilder builder = new StringBuilder();
-                if (!successfullyInstalledFeatures.isEmpty()) {
-                    builder.append("Successfully installed features: ");
-                    builder.append(System.lineSeparator());
-                    for (final FeatureDTO feature : successfullyInstalledFeatures) {
-                        builder.append(featureIdAsString(feature.id));
-                        builder.append(System.lineSeparator());
-                    }
-                } else {
-                    builder.append("No features updated");
-                    builder.append(System.lineSeparator());
-                }
-                if (!notInstalledFeatures.isEmpty()) {
-                    builder.append("Features not updated: ");
-                    builder.append(System.lineSeparator());
-                    for (final FeatureDTO feature : notInstalledFeatures) {
-                        builder.append(featureIdAsString(feature.id));
-                        builder.append(System.lineSeparator());
-                    }
-                }
-                if (!notInstalledFeatures.isEmpty()) {
-                    logger.atWarning().log("Some of the features successfully installed and some not");
-                    FxDialog.showWarningDialog(header, builder.toString(), getClass().getClassLoader());
-                } else {
-                    logger.atInfo().log("All features successfully installed");
-                    storeURL(archiveURL);
-                    FxDialog.showInfoDialog(header, builder.toString(), getClass().getClassLoader());
-                    // show information about the restart of the application
-                    FxDialog.showInfoDialog(header, "The application must be restarted, therefore, will be shut down right away",
-                            getClass().getClassLoader(), btn -> workbench.restart());
-                }
-            }
+				final var builder = new StringBuilder();
+				if (!successfullyInstalledFeatures.isEmpty()) {
+					builder.append("Successfully installed features: ");
+					builder.append(System.lineSeparator());
+					for (final FeatureDTO feature : successfullyInstalledFeatures) {
+						builder.append(featureIdAsString(feature.id));
+						builder.append(System.lineSeparator());
+					}
+				} else {
+					builder.append("No features updated");
+					builder.append(System.lineSeparator());
+				}
+				if (!notInstalledFeatures.isEmpty()) {
+					builder.append("Features not updated: ");
+					builder.append(System.lineSeparator());
+					for (final FeatureDTO feature : notInstalledFeatures) {
+						builder.append(featureIdAsString(feature.id));
+						builder.append(System.lineSeparator());
+					}
+				}
+				if (!notInstalledFeatures.isEmpty()) {
+					logger.atWarning().log("Some of the features successfully installed and some not");
+					FxDialog.showWarningDialog(header, builder.toString(), getClass().getClassLoader());
+				} else {
+					logger.atInfo().log("All features successfully installed");
+					storeURL(archiveURL);
+					FxDialog.showInfoDialog(header, builder.toString(), getClass().getClassLoader());
+					// show information about the restart of the application
+					FxDialog.showInfoDialog(header, "The application must be restarted, therefore, will be shut down right away",
+					        getClass().getClassLoader(), btn -> workbench.restart());
+				}
+			}
 
-            private void storeURL(final String archiveURL) {
-                if (!isWebURL(archiveURL)) {
-                    return;
-                }
-                final Gson  gson = new Gson();
-                Set<String> toBeStored;
-                if (repos.getValue().isEmpty()) {
-                    toBeStored = Sets.newHashSet(archiveURL);
-                } else {
-                    toBeStored = gson.fromJson(repos.getValue(), new TypeToken<HashSet<String>>() {
-                    }.getType());
-                    toBeStored.add(archiveURL);
-                }
-                final String json = gson.toJson(toBeStored);
-                repos.publish(json);
-            }
-        };
+			private void storeURL(final String archiveURL) {
+				if (!isWebURL(archiveURL)) {
+					return;
+				}
+				final var   gson = new Gson();
+				Set<String> toBeStored;
+				if (repos.getValue().isEmpty()) {
+					toBeStored = Sets.newHashSet(archiveURL);
+				} else {
+					toBeStored = gson.fromJson(repos.getValue(), new TypeToken<HashSet<String>>() {
+					}.getType());
+					toBeStored.add(archiveURL);
+				}
+				final var json = gson.toJson(toBeStored);
+				repos.publish(json);
+			}
+		};
 
-        final Thread th = new Thread(task);
-        th.setDaemon(true);
-        th.start();
+		final var th = new Thread(task);
+		th.setDaemon(true);
+		th.start();
 
-        progressDialog = FxDialog.showProgressDialog("External Feature Installation", task, getClass().getClassLoader());
-    }
+		progressDialog = FxDialog.showProgressDialog("External Feature Installation", task, getClass().getClassLoader());
+	}
 
-    private static boolean isWebURL(final String url) {
-        try {
-            new URL(url);
-            return true;
-        } catch (final Exception e) {
-            return false;
-        }
-    }
+	private static boolean isWebURL(final String url) {
+		try {
+			new URL(url);
+			return true;
+		} catch (final Exception e) {
+			return false;
+		}
+	}
 
-    private String featureIdAsString(final IdDTO id) {
-        return id.groupId + ":" + id.artifactId + ":" + id.version;
-    }
+	private String featureIdAsString(final IdDTO id) {
+		return id.groupId + ":" + id.artifactId + ":" + id.version;
+	}
 
 }
