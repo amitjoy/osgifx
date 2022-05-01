9fa11c93f4e61ad7791d43e9ad8f4068c3d6889b
Amit Kumar Mondal <admin@amitinside.com>
2022-05-01 19:00:42 +0200

    Added more defensive programming approach to catch unexpected exceptions

--- com.osgifx.console.ui.packages/src/main/java/com/osgifx/console/ui/packages/PackagesFxController.java
+++ com.osgifx.console.ui.packages/src/main/java/com/osgifx/console/ui/packages/PackagesFxController.java
@@ -68,9 +68,13 @@ public final class PackagesFxController {
 			Fx.addTablePlaceholderWhenDisconnected(table);
 			return;
 		}
-		createControls();
-		Fx.disableSelectionModel(table);
-		logger.atDebug().log("FXML controller has been initialized");
+		try {
+			createControls();
+			Fx.disableSelectionModel(table);
+			logger.atDebug().log("FXML controller has been initialized");
+		} catch (final Exception e) {
+			logger.atError().withException(e).log("FXML controller could not be initialized");
+		}
 	}
 
 	private void createControls() {
