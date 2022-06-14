9f30937ea88969ca59c16ac94f6dfc0519635ff2
Amit Kumar Mondal <admin@amitinside.com>
2022-06-14 11:04:50 +0200

    Introduced Project Templates
    Fixes #132

---
+++ com.osgifx.console.templates/resources/templates/ui/$srcDir$/$basePackageDir$/SampleFxController.java
@@ -0,0 +1,24 @@
+package $basePackageName$;
+
+import javax.inject.Inject;
+
+import org.eclipse.fx.core.log.FluentLogger;
+import org.eclipse.fx.core.log.Log;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public final class SampleFxController {
+
+	@Log
+	@Inject
+	private FluentLogger logger;
+	@FXML
+	private AnchorPane   rootPane;
+
+	@FXML
+	public void initialize() {
+		logger.atDebug().log("FXML controller has been initialized");
+	}
+
+}
