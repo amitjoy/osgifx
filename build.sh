0c4b48cf701185dac27e3a9f5dbbefa3c969656b
Amit Kumar Mondal <admin@amitinside.com>
2022-06-24 00:00:21 +0200

    Remove jpackage integration
    Fixes #229

--- build.sh
+++
@@ -1,4 +0,0 @@
-./gradlew clean build --max-workers=5 --parallel
-./gradlew :com.osgifx.console.product:export.osgifx
-cp com.osgifx.console.product/target/distributions/executable/osgifx.jar com.osgifx.console.dist/uberjar
-./gradlew jpackage --info
