3d88cc7e2d8c42f6b21685ed675bb68e3bbdb4a8
Amit Kumar Mondal <admin@amitinside.com>
2022-07-07 07:17:07 +0200

    Added unicode emoji to commit

--- scripts/prepare_for_next_sprint.sh
+++ scripts/prepare_for_next_sprint.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Updating OSGi.fx Application Version to $1"
-echo "Updating OSGi.fx Baseline Version to $2"
+echo "🏁 Updating OSGi.fx Application Version to $1"
+echo "🏁 Updating OSGi.fx Baseline Version to $2"
 
 ./scripts/update_version.sh $1 $2
 
-echo "Committing the changes to current branch"
+echo "🏁 Committing the changes to current branch"
 
 git add .
-git commit -m "Next Development Sprint Preparation"
+git commit -m "🏁 Next Development Sprint Preparation"
