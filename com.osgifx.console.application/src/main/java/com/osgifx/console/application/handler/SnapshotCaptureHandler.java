a0af9accbb9950fc334de98ac30daad529ee1504
Amit Kumar Mondal <admin@amitinside.com>
2022-11-01 08:36:32 +0100

    Added Dummy Snapshot Handlers

---
+++ com.osgifx.console.application/src/main/java/com/osgifx/console/application/handler/SnapshotCaptureHandler.java
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ * Copyright 2021-2022 Amit Kumar Mondal
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
+package com.osgifx.console.application.handler;
+
+import java.awt.Desktop;
+import java.io.IOException;
+
+import javax.inject.Inject;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.core.di.extensions.OSGiBundle;
+import org.eclipse.fx.core.log.FluentLogger;
+import org.eclipse.fx.core.log.Log;
+import org.osgi.framework.BundleContext;
+
+import com.osgifx.console.log.DiagnosticsAdmin;
+
+public final class SnapshotCaptureHandler {
+
+    @Log
+    @Inject
+    private FluentLogger     logger;
+    @Inject
+    @OSGiBundle
+    private BundleContext    context;
+    @Inject
+    private DiagnosticsAdmin diagnosticsAdmin;
+
+    @Execute
+    public void execute() {
+        try {
+            Desktop.getDesktop().open(diagnosticsAdmin.getLogFilesDirectory());
+            logger.atInfo().log("Diagnostics directory has been opened");
+        } catch (final IOException e) {
+            logger.atError().withException(e).log("Cannot open diagnostics directory");
+        }
+    }
+
+}
