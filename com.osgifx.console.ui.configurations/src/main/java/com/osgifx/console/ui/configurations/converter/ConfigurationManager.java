91547ef4271d4d5501b1ed70b901f0cf7be46674
Amit Kumar Mondal <admin@amitinside.com>
2022-11-14 21:37:47 +0100

    Fixed NPE due to missing service dependency
    Fixes #341

--- com.osgifx.console.ui.configurations/src/main/java/com/osgifx/console/ui/configurations/converter/ConfigurationManager.java
+++ com.osgifx.console.ui.configurations/src/main/java/com/osgifx/console/ui/configurations/converter/ConfigurationManager.java
@@ -15,6 +15,9 @@
  ******************************************************************************/
 package com.osgifx.console.ui.configurations.converter;
 
+import static org.osgi.service.component.annotations.ReferenceCardinality.OPTIONAL;
+import static org.osgi.service.component.annotations.ReferencePolicyOption.GREEDY;
+
 import java.util.List;
 
 import org.eclipse.fx.core.log.FluentLogger;
@@ -22,6 +25,7 @@ import org.eclipse.fx.core.log.LoggerFactory;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
+import com.osgifx.console.agent.Agent;
 import com.osgifx.console.agent.dto.ConfigValue;
 import com.osgifx.console.agent.dto.XResultDTO;
 import com.osgifx.console.supervisor.Supervisor;
@@ -30,19 +34,19 @@ import com.osgifx.console.supervisor.Supervisor;
 public final class ConfigurationManager {
 
     @Reference
-    private LoggerFactory factory;
-    @Reference
-    private Supervisor    supervisor;
-    private FluentLogger  logger;
+    private LoggerFactory       factory;
+    @Reference(cardinality = OPTIONAL, policyOption = GREEDY)
+    private volatile Supervisor supervisor;
+    private FluentLogger        logger;
 
     void activate() {
         logger = FluentLogger.of(factory.createLogger(getClass().getName()));
     }
 
     public boolean createOrUpdateConfiguration(final String pid, final List<ConfigValue> newProperties) {
-        final var agent = supervisor.getAgent();
-        if (agent == null) {
-            logger.atWarning().log("Remote agent cannot be connected");
+        Agent agent = null;
+        if (supervisor == null || (agent = supervisor.getAgent()) == null) {
+            logger.atWarning().log("Agent is not connected");
             return false;
         }
         final var result = agent.createOrUpdateConfiguration(pid, newProperties);
@@ -50,9 +54,9 @@ public final class ConfigurationManager {
     }
 
     public boolean createFactoryConfiguration(final String factoryPid, final List<ConfigValue> newProperties) {
-        final var agent = supervisor.getAgent();
-        if (agent == null) {
-            logger.atWarning().log("Remote agent cannot be connected");
+        Agent agent = null;
+        if (supervisor == null || (agent = supervisor.getAgent()) == null) {
+            logger.atWarning().log("Agent is not connected");
             return false;
         }
         final var result = agent.createFactoryConfiguration(factoryPid, newProperties);
