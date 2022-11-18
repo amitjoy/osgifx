20f7b5d5c527a48e996aee1a4e21f8d261108e3a
Amit Kumar Mondal <admin@amitinside.com>
2022-11-18 02:19:29 +0100

    Added required changes for deploying to maven central

--- scripts/prepare_dev.sh
+++ scripts/prepare_dev.sh
@@ -4,9 +4,11 @@ set -e
 echo "🏁 Updating OSGi.fx Application Version to $1"
 echo "🏁 Updating OSGi.fx Baseline Version to $2"
 
-./scripts/update_version.sh $1 $2
+echo $1 > cnf/version/app.version
+echo $2 > cnf/version/baseline.version
+npm version $1 --no-git-tag-version
 
 echo "🏁 Committing the changes to current branch"
 
 git add .
-git commit -m "🏁 Next Development Sprint Preparation"
+git commit -m "🏁 Next Development Cycle Preparation"
