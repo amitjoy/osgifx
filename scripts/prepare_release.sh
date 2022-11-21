9e3b883fabeb83a5b06b85cbca7adaf66bf74849
Amit Kumar Mondal <admin@amitinside.com>
2022-11-21 12:52:23 +0100

    Fixed issue with release preparation script
    Fixes #361

--- scripts/prepare_release.sh
+++ scripts/prepare_release.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
-echo "🏁 Retrieving version"
+echo "🏁 Retrieving Version"
 version=$(cat cnf/version/app.version)
 
 echo "🏁 Updating Version"
 version_without_snapshot=${version%".SNAPSHOT"}
 echo $version_without_snapshot > cnf/version/app.version
 
-echo "🏁 Creating tag: v$version"
-git tag v$version
+echo "🏁 Committing Changes"
+git add .
+git commit -m "🏁 REL v$version_without_snapshot Preparation"
+
+echo "🏁 Creating Tag: v$c"
+git tag v$version_without_snapshot
