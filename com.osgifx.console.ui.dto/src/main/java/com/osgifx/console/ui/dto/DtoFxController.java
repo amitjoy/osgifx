898f0f8462c95972f8c06eb581fdab7a7445156a
Amit Kumar Mondal <admin@amitinside.com>
2022-12-12 21:56:45 +0100

    Added responsiveness to expand and collapse tasks
    fixes #479

--- com.osgifx.console.ui.dto/src/main/java/com/osgifx/console/ui/dto/DtoFxController.java
+++ com.osgifx.console.ui.dto/src/main/java/com/osgifx/console/ui/dto/DtoFxController.java
@@ -25,13 +25,15 @@ import org.eclipse.fx.core.log.Log;
 import org.eclipse.fx.ui.controls.tree.FilterableTreeItem;
 import org.eclipse.fx.ui.controls.tree.TreeItemPredicate;
 
+import com.google.common.base.Strings;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.osgifx.console.data.provider.DataProvider;
+import com.osgifx.console.executor.Executor;
 
-import javafx.beans.binding.Bindings;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -43,6 +45,10 @@ public final class DtoFxController {
     @Log
     @Inject
     private FluentLogger      logger;
+    @Inject
+    private Executor          executor;
+    @FXML
+    private Button            searchBtn;
     @FXML
     private Button            expandBtn;
     @FXML
@@ -90,19 +96,28 @@ public final class DtoFxController {
 
         final var root = parseJSON("ROOT", json);
         dtoTree.setRoot(root);
-        addSearchBinding(root);
 
-        expandBtn.setOnMouseClicked(event -> expandOrCollapseTreeView(root, true));
-        collapseBtn.setOnMouseClicked(event -> expandOrCollapseTreeView(root, false));
-    }
+        expandBtn.setOnMouseClicked(event -> expandOrCollapseTask(root, true));
+        collapseBtn.setOnMouseClicked(event -> expandOrCollapseTask(root, false));
+        searchBtn.setOnMouseClicked(event -> {
+            final Task<Void> task = new Task<>() {
 
-    private void addSearchBinding(final FilterableTreeItem<String> treeItem) {
-        treeItem.predicateProperty().bind(Bindings.createObjectBinding(() -> {
-            if (searchBox.getText() == null || searchBox.getText().isEmpty()) {
-                return null;
-            }
-            return TreeItemPredicate.create(itemText -> StringUtils.containsIgnoreCase(itemText, searchBox.getText()));
-        }, searchBox.textProperty()));
+                @Override
+                protected Void call() throws Exception {
+                    threadSync.asyncExec(() -> {
+                        final var itemText = searchBox.getText();
+                        if (Strings.isNullOrEmpty(itemText)) {
+                            root.setPredicate(null);
+                        } else {
+                            root.setPredicate(TreeItemPredicate
+                                    .create(item -> StringUtils.containsIgnoreCase(item, searchBox.getText())));
+                        }
+                    });
+                    return null;
+                }
+            };
+            executor.runAsync(task);
+        });
     }
 
     private static FilterableTreeItem<String> parseJSON(final String name, final Object json) {
@@ -128,6 +143,18 @@ public final class DtoFxController {
             item.setValue(name + " : " + json);
         }
         return item;
+    }
+
+    private void expandOrCollapseTask(final TreeItem<?> item, final boolean expand) {
+        final Task<Void> task = new Task<>() {
+
+            @Override
+            protected Void call() throws Exception {
+                expandOrCollapseTreeView(item, expand);
+                return null;
+            }
+        };
+        executor.runAsync(task);
     }
 
     private void expandOrCollapseTreeView(final TreeItem<?> item, final boolean expand) {
