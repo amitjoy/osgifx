65f5832e586cc35cbd32195781e8eb162ca56ac6
Amit Kumar Mondal <admin@amitinside.com>
2023-05-19 02:33:07 +0200

    Simplified Wrapping MQTT APIs in Agent and in Supervisor

--- com.osgifx.console.agent.api/src/main/java/com/osgifx/console/agent/rpc/mqtt/InterruptSafe.java
+++ com.osgifx.console.agent.api/src/main/java/com/osgifx/console/agent/rpc/mqtt/InterruptSafe.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  ******************************************************************************/
-package com.osgifx.console.agent.helper;
+package com.osgifx.console.agent.rpc.mqtt;
 
 import java.util.concurrent.Callable;
 
