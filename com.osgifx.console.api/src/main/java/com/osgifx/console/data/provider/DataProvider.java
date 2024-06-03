278a91cfb5141954717c51b101c0916d017ab9ad
Amit Kumar Mondal <admin@amitinside.com>
2024-06-03 11:12:02 +0200

    Updated DataProvider API Javadoc

--- com.osgifx.console.api/src/main/java/com/osgifx/console/data/provider/DataProvider.java
+++ com.osgifx.console.api/src/main/java/com/osgifx/console/data/provider/DataProvider.java
@@ -1,15 +1,15 @@
 /*******************************************************************************
- * Copyright 2021-2024 Amit Kumar Mondal
+ * COPYRIGHT 2021-2024 AMIT KUMAR MONDAL
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License.  You may obtain a copy
- * of the License at
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at:
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
  * the License.
  ******************************************************************************/
@@ -39,148 +39,143 @@ import com.osgifx.console.agent.dto.XThreadDTO;
 import javafx.collections.ObservableList;
 
 /**
- * This service is responsible for providing the utility methods to retrieve
- * data from the runtime so that they can be shown on the UI
+ * Provides utility methods for retrieving data from the runtime for display on the UI.
  */
 @ProviderType
 public interface DataProvider {
 
     /**
-     * Retrieves information from the remote runtime
+     * Retrieves information from the remote runtime based on the provided identifier.
      *
-     * @param id the identifier to execute a specific information, otherwise,
-     *            set to {@code null} to retrieve all informations
-     * @param isAsync set to {@code true} if the information needs to be retrieved
-     *            asynchronously
+     * @param id the identifier for the specific information to retrieve, or {@code null} to retrieve all information
+     * @param isAsync {@code true} if the information should be retrieved asynchronously, otherwise {@code false}
      */
     void retrieveInfo(String id, boolean isAsync);
 
     /**
-     * Returns the observable list of bundles
+     * Returns an observable list of bundles.
      *
      * @return the observable list of bundles
      */
     ObservableList<XBundleDTO> bundles();
 
     /**
-     * Returns the observable list of packages
+     * Returns an observable list of packages.
      *
      * @return the observable list of packages
      */
     ObservableList<PackageDTO> packages();
 
     /**
-     * Returns the observable list of services
+     * Returns an observable list of services.
      *
      * @return the observable list of services
      */
     ObservableList<XServiceDTO> services();
 
     /**
-     * Returns the observable list of components
+     * Returns an observable list of components.
      *
      * @return the observable list of components
      */
     ObservableList<XComponentDTO> components();
 
     /**
-     * Returns the observable list of configurations
+     * Returns an observable list of configurations.
      *
      * @return the observable list of configurations
      */
     ObservableList<XConfigurationDTO> configurations();
 
     /**
-     * Returns the observable list of events
+     * Returns an observable list of events.
      *
      * @return the observable list of events
      */
     ObservableList<XEventDTO> events();
 
     /**
-     * Returns the observable list of logs
+     * Returns an observable list of logs.
      *
      * @return the observable list of logs
      */
     ObservableList<XLogEntryDTO> logs();
 
     /**
-     * Returns the observable list of properties
+     * Returns an observable list of properties.
      *
      * @return the observable list of properties
      */
     ObservableList<XPropertyDTO> properties();
 
     /**
-     * Returns the observable list of threads
+     * Returns an observable list of threads.
      *
      * @return the observable list of threads
      */
     ObservableList<XThreadDTO> threads();
 
     /**
-     * Returns the observable list of classloader leaks
+     * Returns an observable list of classloader leaks.
      *
      * @return the observable list of classloader leaks
      */
-
     ObservableList<XBundleDTO> leaks();
 
     /**
-     * Returns the HTTP runtime information that includes list of all servlets,
-     * resources, listeners, filters and error pages
+     * Returns HTTP runtime information, including lists of all servlets, resources, listeners, filters, and error pages.
      *
      * @return the observable list of all HTTP components
      */
     ObservableList<XHttpComponentDTO> httpComponents();
 
     /**
-     * Returns the Felix health checks
+     * Returns the Felix health checks.
      *
      * @return the observable list of all Felix health checks
      */
     ObservableList<XHealthCheckDTO> healthchecks();
 
     /**
-     * Returns the different roles (users and groups) stored in {@code UserAdmin}
+     * Returns the roles (users and groups) stored in {@code UserAdmin}.
      *
      * @return the observable list of all roles
      */
     ObservableList<XRoleDTO> roles();
 
     /**
-     * Returns the different logger contexts
+     * Returns the logger contexts.
      *
      * @return the observable list of all logger contexts
      */
     ObservableList<XBundleLoggerContextDTO> loggerContexts();
 
     /**
-     * Returns the memory information of the remote runtime
+     * Returns the memory information of the remote runtime.
      *
-     * @return the memory information
+     * @return a CompletableFuture containing the memory information
      */
     CompletableFuture<XMemoryInfoDTO> memory();
 
     /**
-     * Returns the DMT node information of the specified node
+     * Returns the DMT node information of the specified node.
      *
-     * @return the DMT node information
+     * @param rootURI the root URI of the node to read
+     * @return a CompletableFuture containing the DMT node information
      */
     CompletableFuture<XDmtNodeDTO> readDmtNode(String rootURI);
 
     /**
-     * Returns the runtime DTOs together
+     * Returns the runtime DTOs together.
      *
-     * @return the runtime DTOs together
+     * @return a CompletableFuture containing the runtime DTOs
      */
     CompletableFuture<RuntimeDTO> readRuntimeDTO();
 
     /**
-     * Returns the heap usage information
+     * Returns the heap usage information.
      *
-     * @return the heap usage information
+     * @return a CompletableFuture containing the heap usage information
      */
     CompletableFuture<XHeapUsageDTO> heapUsage();
-
 }
