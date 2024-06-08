f149bf334157f7f203edef232480abd0b09a2c31
Amit Kumar Mondal <admin@amitinside.com>
2024-06-08 11:24:22 +0200

    Added comments to BundleDTO

--- com.osgifx.console.agent.api/src/main/java/com/osgifx/console/agent/dto/XBundleDTO.java
+++ com.osgifx.console.agent.api/src/main/java/com/osgifx/console/agent/dto/XBundleDTO.java
@@ -1,15 +1,15 @@
 /*******************************************************************************
- * Copyright 2021-2024 Amit Kumar Mondal
+ * COPYRIGHT 2021-2024 AMIT KUMAR MONDAL
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License.  You may obtain a copy
- * of the License at
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at:
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
  * the License.
  ******************************************************************************/
@@ -21,35 +21,93 @@ import java.util.Map;
 import org.osgi.dto.DTO;
 import org.osgi.framework.wiring.dto.BundleRevisionDTO;
 
+/**
+ * Data Transfer Object (DTO) representing information about an OSGi bundle.
+ */
 public class XBundleDTO extends DTO {
 
-    public long                  id;
-    public String                state;
-    public String                location;
-    public String                category;
-    public int                   revisions;
-    public boolean               isFragment;
-    public long                  lastModified;
-    public long                  dataFolderSize;
-    public String                documentation;
-    public String                vendor;
-    public String                version;
-    public String                description;
-    public int                   startLevel;
-    public int                   frameworkStartLevel;
-    public String                symbolicName;
-    public long                  startDurationInMillis;
-    public BundleRevisionDTO     bundleRevision;
-    public List<XPackageDTO>     exportedPackages;
-    public List<XPackageDTO>     importedPackages;
-    public List<XBundleInfoDTO>  wiredBundlesAsProvider;
-    public List<XBundleInfoDTO>  wiredBundlesAsRequirer;
+    /** Unique identifier of the bundle. */
+    public long id;
+
+    /** Current state of the bundle. */
+    public String state;
+
+    /** Location of the bundle. */
+    public String location;
+
+    /** Category of the bundle. */
+    public String category;
+
+    /** Number of revisions of the bundle. */
+    public int revisions;
+
+    /** Indicates if the bundle is a fragment. */
+    public boolean isFragment;
+
+    /** Timestamp of the last modification of the bundle. */
+    public long lastModified;
+
+    /** Size of the bundle's data folder. */
+    public long dataFolderSize;
+
+    /** Documentation associated with the bundle. */
+    public String documentation;
+
+    /** Vendor of the bundle. */
+    public String vendor;
+
+    /** Version of the bundle. */
+    public String version;
+
+    /** Description of the bundle. */
+    public String description;
+
+    /** Start level of the bundle. */
+    public int startLevel;
+
+    /** Framework start level of the bundle. */
+    public int frameworkStartLevel;
+
+    /** Symbolic name of the bundle. */
+    public String symbolicName;
+
+    /** Duration taken to start the bundle in milliseconds. */
+    public long startDurationInMillis;
+
+    /** Detailed information about the bundle's revision. */
+    public BundleRevisionDTO bundleRevision;
+
+    /** Packages exported by the bundle. */
+    public List<XPackageDTO> exportedPackages;
+
+    /** Packages imported by the bundle. */
+    public List<XPackageDTO> importedPackages;
+
+    /** Bundles wired as providers by the bundle. */
+    public List<XBundleInfoDTO> wiredBundlesAsProvider;
+
+    /** Bundles wired as requirers by the bundle. */
+    public List<XBundleInfoDTO> wiredBundlesAsRequirer;
+
+    /** Services registered by the bundle. */
     public List<XServiceInfoDTO> registeredServices;
-    public Map<String, String>   manifestHeaders;
+
+    /** Manifest headers of the bundle. */
+    public Map<String, String> manifestHeaders;
+
+    /** Services used by the bundle. */
     public List<XServiceInfoDTO> usedServices;
-    public List<XBundleInfoDTO>  hostBundles;
-    public List<XBundleInfoDTO>  fragmentsAttached;
-    public boolean               isPersistentlyStarted;
-    public boolean               isActivationPolicyUsed;
+
+    /** Host bundles related to the bundle. */
+    public List<XBundleInfoDTO> hostBundles;
+
+    /** Fragments attached to the bundle. */
+    public List<XBundleInfoDTO> fragmentsAttached;
+
+    /** Indicates if the bundle is persistently started. */
+    public boolean isPersistentlyStarted;
+
+    /** Indicates if activation policy is used by the bundle. */
+    public boolean isActivationPolicyUsed;
 
 }
