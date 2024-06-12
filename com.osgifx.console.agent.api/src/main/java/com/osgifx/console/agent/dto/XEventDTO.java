0a1110788ed028af6dd7b4eb3a3f5147fef282b3
Amit Kumar Mondal <admin@amitinside.com>
2024-06-12 12:06:45 +0200

    Added Javadoc to EventDTO

--- com.osgifx.console.agent.api/src/main/java/com/osgifx/console/agent/dto/XEventDTO.java
+++ com.osgifx.console.agent.api/src/main/java/com/osgifx/console/agent/dto/XEventDTO.java
@@ -2,27 +2,38 @@
  * Copyright 2021-2024 Amit Kumar Mondal
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License.  You may obtain a copy
+ * use this file except in compliance with the License. You may obtain a copy
  * of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
  * the License.
  ******************************************************************************/
+
 package com.osgifx.console.agent.dto;
 
 import java.util.Map;
 
 import org.osgi.dto.DTO;
 
+/**
+ * A data transfer object (DTO) representing an event message.
+ * It encapsulates information about the event topic, received timestamp,
+ * and additional properties associated with the event.
+ */
 public class XEventDTO extends DTO {
 
-    public String              topic;
-    public long                received;
+    /** The topic of the event */
+    public String topic;
+
+    /** The timestamp when the event was received */
+    public long received;
+
+    /** Additional properties associated with the event */
     public Map<String, String> properties;
 
 }
