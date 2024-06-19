5a0fbfec9ce8d696dc2efb8428d51f8aa7b9bf2a
Amit Kumar Mondal <admin@amitinside.com>
2024-06-19 12:18:23 +0200

    Updated ServiceDTO with missing comments

--- com.osgifx.console.agent.api/src/main/java/com/osgifx/console/agent/dto/XServiceDTO.java
+++ com.osgifx.console.agent.api/src/main/java/com/osgifx/console/agent/dto/XServiceDTO.java
@@ -2,17 +2,18 @@
  * Copyright 2021-2024 Amit Kumar Mondal
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License.  You may obtain a copy
+ * use this file except in compliance with the License. You may obtain a copy
  * of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
  * the License.
  ******************************************************************************/
+
 package com.osgifx.console.agent.dto;
 
 import java.util.List;
@@ -20,13 +21,30 @@ import java.util.Map;
 
 import org.osgi.dto.DTO;
 
+/**
+ * A data transfer object (DTO) representing an OSGi service.
+ * 
+ * It encapsulates information about the service ID, types, bundle ID,
+ * properties, bundles using the service, and the registering bundle.
+ */
 public class XServiceDTO extends DTO {
 
-    public long                 id;
-    public List<String>         types;
-    public long                 bundleId;
-    public Map<String, String>  properties;
+    /** The ID of the OSGi service */
+    public long id;
+
+    /** The types of the OSGi service */
+    public List<String> types;
+
+    /** The ID of the bundle registering the service */
+    public long bundleId;
+
+    /** The properties associated with the service */
+    public Map<String, String> properties;
+
+    /** The list of bundles using this service */
     public List<XBundleInfoDTO> usingBundles;
-    public String               registeringBundle;
+
+    /** The symbolic name of the bundle that registered the service */
+    public String registeringBundle;
 
 }
