7960ce82bfd03904ea4cc7d385299b1501544267
Amit Kumar Mondal <admin@amitinside.com>
2024-08-04 11:48:40 +0200

    Added missing comment to Mqtt5Subscriber

--- com.osgifx.console.agent.api/src/main/java/com/osgifx/console/agent/rpc/mqtt/api/Mqtt5Subscriber.java
+++ com.osgifx.console.agent.api/src/main/java/com/osgifx/console/agent/rpc/mqtt/api/Mqtt5Subscriber.java
@@ -5,7 +5,7 @@
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
@@ -18,16 +18,30 @@ package com.osgifx.console.agent.rpc.mqtt.api;
 import org.osgi.util.pushstream.PushStream;
 
 /**
- * The consumer interface to plugin custom functionality for consuming MQTT messages
+ * Represents a subscriber for MQTT version 5 messages. This interface allows for 
+ * the implementation of custom functionality to consume messages from specified 
+ * MQTT topics.
+ * <p>
+ * Implementations of this interface are responsible for subscribing to a given 
+ * topic and providing a mechanism to receive and process messages asynchronously 
+ * using a {@link PushStream}.
+ * </p>
  */
 public interface Mqtt5Subscriber {
 
     /**
-     * Subscribe to the specified topic
+     * Subscribes to the specified MQTT topic and returns a {@link PushStream} that 
+     * allows asynchronous processing of incoming messages from that topic.
+     * <p>
+     * The topic parameter identifies the channel to which the subscriber will listen 
+     * for incoming messages.
+     * </p>
      *
-     * @param topic the topic to subscribe to
-     * @return {@link PushStream} instance for the subscription
+     * @param topic the MQTT topic to subscribe to, must not be null or empty
+     * @return a {@link PushStream} instance representing the stream of messages 
+     *         received from the subscribed topic
+     * @throws IllegalArgumentException if the topic is null or empty
      */
-    PushStream<Mqtt5Message> subscribe(String channel);
+    PushStream<Mqtt5Message> subscribe(String topic);
 
 }
