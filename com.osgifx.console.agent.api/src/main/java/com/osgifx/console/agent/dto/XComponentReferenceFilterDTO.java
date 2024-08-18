6245f0d1bb2a022c129ac675d6d196124ed46de8
Amit Kumar Mondal <admin@amitinside.com>
2024-08-18 12:38:57 +0200

    Added necessary comments to XComponentReferenceFilterDTO

--- com.osgifx.console.agent.api/src/main/java/com/osgifx/console/agent/dto/XComponentReferenceFilterDTO.java
+++ com.osgifx.console.agent.api/src/main/java/com/osgifx/console/agent/dto/XComponentReferenceFilterDTO.java
@@ -5,7 +5,7 @@
  * use this file except in compliance with the License.  You may obtain a copy
  * of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
@@ -17,10 +17,26 @@ package com.osgifx.console.agent.dto;
 
 import org.osgi.dto.DTO;
 
+/**
+ * Represents a filter for a component reference within the OSGi framework.
+ * This class extends the {@link DTO} class to provide a standardized data transfer 
+ * object for defining filters that are applied to component references.
+ * <p>
+ * The {@code XComponentReferenceFilterDTO} class contains information such as 
+ * the component name, the target key for filtering, and the filter expression 
+ * to be applied. It is used to specify criteria for selecting or matching 
+ * references in the OSGi service registry.
+ * </p>
+ */
 public class XComponentReferenceFilterDTO extends DTO {
 
+    /** The name of the component to which this filter applies. */
     public String componentName;
+
+    /** The key used to target a specific reference for filtering. */
     public String targetKey;
+
+    /** The LDAP-style filter expression used to filter the component reference. */
     public String targetFilter;
 
 }
