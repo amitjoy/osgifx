e6fc528ed9358158b2a7e50bd04edfc207232793
Amit Kumar Mondal <admin@amitinside.com>
2024-08-26 12:52:54 +0200

    Added API comments

--- com.osgifx.console.agent.api/src/main/java/com/osgifx/console/agent/dto/XResultDTO.java
+++ com.osgifx.console.agent.api/src/main/java/com/osgifx/console/agent/dto/XResultDTO.java
@@ -5,7 +5,7 @@
  * use this file except in compliance with the License.  You may obtain a copy
  * of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
@@ -17,13 +17,30 @@ package com.osgifx.console.agent.dto;
 
 import org.osgi.dto.DTO;
 
+/**
+ * Represents a result within the OSGi framework. This class extends the {@link DTO} 
+ * class to provide a standardized data transfer object for result-related information.
+ * <p>
+ * The {@code XResultDTO} class includes details such as the result status and a 
+ * response message. It is used to transfer result data between different components 
+ * or systems in a consistent format.
+ * </p>
+ */
 public class XResultDTO extends DTO {
 
+    /** Constant representing a successful result. */
     public static final int SUCCESS = 1;
-    public static final int ERROR   = 2;
+
+    /** Constant representing an error result. */
+    public static final int ERROR = 2;
+
+    /** Constant representing a skipped result. */
     public static final int SKIPPED = 3;
 
-    public int    result;
+    /** The result status (e.g., SUCCESS, ERROR, SKIPPED). */
+    public int result;
+
+    /** The response message associated with the result. */
     public String response;
 
 }
