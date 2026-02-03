e256d5fc444f1c886b1967c3b7cddecb221d80ff
Amit Kumar Mondal <admin@amitinside.com>
2026-02-04 00:18:50 +0100

    [FIX-782] Update copyright year
    Adjusts the copyright year in all relevant source files from 2025 to 2026.
    
    fixes #782

--- com.osgifx.console.api/src/main/java/com/osgifx/console/executor/Executor.java
+++ com.osgifx.console.api/src/main/java/com/osgifx/console/executor/Executor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * COPYRIGHT 2021-2025 AMIT KUMAR MONDAL
+ * Copyright 2021-2026 Amit Kumar Mondal
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy
