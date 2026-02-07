a64a05814eca4937a079a8f25b2eb3fabce49a76
Amit Kumar Mondal <admin@amitinside.com>
2026-02-07 08:34:11 +0100

    Adds headless agent connection mode
    Introduces a headless launch addon enabling automatic connection to OSGi agents without a UI.
    
    The application can now parse connection details from a JSON configuration file, specified by the `osgifx.config` system property. This supports both Socket and MQTT connection types, facilitating automation for scripting or CI/CD environments. Dedicated DTOs are included to structure the headless configuration.
    
    Also adds missing license headers to several files.
    
    fixes #798

---
+++ com.osgifx.console.application/src/main/java/com/osgifx/console/application/dto/HeadlessConfigDTO.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright 2021-2026 Amit Kumar Mondal
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
+package com.osgifx.console.application.dto;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+public class HeadlessConfigDTO {
+
+    public HeadlessConnectionType type;
+    public SocketConfigDTO        socket;
+    public MqttConfigDTO          mqtt;
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.JSON_STYLE);
+    }
+
+}
