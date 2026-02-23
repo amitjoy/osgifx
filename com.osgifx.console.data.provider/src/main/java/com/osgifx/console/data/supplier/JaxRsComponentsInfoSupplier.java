17ffceaf0dcba708cc86b3d1b9e3fe0e4cd8cf8c
Amit Kumar Mondal <admin@amitinside.com>
2026-02-24 00:59:42 +0100

    Added JAX-RS support to the console
    Introduced a new UI module and backend infrastructure to visualize JAX-RS Whiteboard components, including applications, extensions, and resources.
    
    Implemented the agent logic to retrieve JAX-RS runtime metadata and updated the snapshot functionality to include this data in framework captures.
    
    Extended the data provider and event system to handle JAX-RS DTOs and lifecycle events, allowing for real-time monitoring of RESTful services within the OSGi environment.
    
    Closes #871

---
+++ com.osgifx.console.data.provider/src/main/java/com/osgifx/console/data/supplier/JaxRsComponentsInfoSupplier.java
@@ -0,0 +1,118 @@
+/*******************************************************************************
+ * Copyright 2021-2026 Amit Kumar Mondal
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
+package com.osgifx.console.data.supplier;
+
+import static com.osgifx.console.data.supplier.JaxRsComponentsInfoSupplier.JAXRS_ID;
+import static com.osgifx.console.event.topics.BundleActionEventTopics.BUNDLE_ACTION_EVENT_TOPICS;
+import static com.osgifx.console.event.topics.ComponentActionEventTopics.COMPONENT_ACTION_EVENT_TOPICS;
+import static com.osgifx.console.event.topics.ConfigurationActionEventTopics.CONFIGURATION_ACTION_EVENT_TOPICS;
+import static com.osgifx.console.event.topics.DataRetrievedEventTopics.DATA_RETRIEVED_JAXRS_TOPIC;
+import static com.osgifx.console.supervisor.Supervisor.AGENT_DISCONNECTED_EVENT_TOPIC;
+import static com.osgifx.console.util.fx.ConsoleFxHelper.makeNullSafe;
+import static javafx.collections.FXCollections.observableArrayList;
+import static org.osgi.service.component.annotations.ReferenceCardinality.OPTIONAL;
+import static org.osgi.service.component.annotations.ReferencePolicyOption.GREEDY;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+import org.eclipse.fx.core.ThreadSynchronize;
+import org.eclipse.fx.core.log.FluentLogger;
+import org.eclipse.fx.core.log.LoggerFactory;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.propertytypes.ServiceRanking;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventAdmin;
+import org.osgi.service.event.EventHandler;
+import org.osgi.service.event.propertytypes.EventTopics;
+
+import com.osgifx.console.agent.dto.XJaxRsComponentDTO;
+import com.osgifx.console.data.manager.RuntimeInfoSupplier;
+import com.osgifx.console.executor.Executor;
+import com.osgifx.console.supervisor.Supervisor;
+
+import javafx.collections.ObservableList;
+
+@Component
+@ServiceRanking(110)
+@SupplierID(JAXRS_ID)
+// @formatter:off
+@EventTopics({
+    AGENT_DISCONNECTED_EVENT_TOPIC,
+    BUNDLE_ACTION_EVENT_TOPICS,
+    COMPONENT_ACTION_EVENT_TOPICS,
+    CONFIGURATION_ACTION_EVENT_TOPICS })
+// @formatter:on
+public final class JaxRsComponentsInfoSupplier implements RuntimeInfoSupplier, EventHandler {
+
+    public static final String JAXRS_ID = "jaxrs";
+
+    @Reference
+    private LoggerFactory       factory;
+    @Reference
+    private Executor            executor;
+    @Reference
+    private EventAdmin          eventAdmin;
+    @Reference
+    private ThreadSynchronize   threadSync;
+    @Reference(cardinality = OPTIONAL, policyOption = GREEDY)
+    private volatile Supervisor supervisor;
+    private FluentLogger        logger;
+
+    private final ObservableList<XJaxRsComponentDTO> jaxRsComponents = observableArrayList();
+    private final ReentrantLock                      retrieveLock    = new ReentrantLock();
+
+    @Activate
+    void activate() {
+        logger = FluentLogger.of(factory.createLogger(getClass().getName()));
+    }
+
+    @Override
+    public void retrieve() {
+        retrieveLock.lock();
+        try {
+            final var agent = supervisor.getAgent();
+            if (agent == null) {
+                logger.atWarning().log("Agent not connected");
+                return;
+            }
+            logger.atInfo().log("Retrieving JAX-RS components info from remote runtime");
+            final var data = makeNullSafe(agent.getJaxRsComponents());
+            threadSync.asyncExec(() -> {
+                jaxRsComponents.setAll(data);
+                RuntimeInfoSupplier.sendEvent(eventAdmin, DATA_RETRIEVED_JAXRS_TOPIC);
+                logger.atInfo().log("JAX-RS components info retrieved successfully");
+            });
+        } finally {
+            retrieveLock.unlock();
+        }
+    }
+
+    @Override
+    public ObservableList<?> supply() {
+        return jaxRsComponents;
+    }
+
+    @Override
+    public void handleEvent(final Event event) {
+        if (AGENT_DISCONNECTED_EVENT_TOPIC.equals(event.getTopic())) {
+            threadSync.asyncExec(jaxRsComponents::clear);
+            return;
+        }
+        executor.runAsync(this::retrieve);
+    }
+}
