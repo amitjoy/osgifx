dd2ef65a4f8d02b589ecc380d47514d5560e976c
Amit Kumar Mondal <admin@amitinside.com>
2026-04-06 12:02:47 +0200

    Added TableView for Remote Services

---
+++ com.osgifx.console.ui.rsa/src/main/java/com/osgifx/console/ui/rsa/RsaFxController.java
@@ -0,0 +1,141 @@
+/*******************************************************************************
+ * Copyright 2021-2026 Amit Kumar Mondal
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
+package com.osgifx.console.ui.rsa;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.controlsfx.control.table.TableFilter;
+import org.controlsfx.control.table.TableRowExpanderColumn;
+import org.controlsfx.control.table.TableRowExpanderColumn.TableRowDataFeatures;
+import org.eclipse.e4.core.di.extensions.OSGiBundle;
+import org.eclipse.fx.core.ThreadSynchronize;
+import org.eclipse.fx.core.di.LocalInstance;
+import org.eclipse.fx.core.log.FluentLogger;
+import org.eclipse.fx.core.log.Log;
+import org.osgi.framework.BundleContext;
+
+import com.osgifx.console.agent.dto.XRemoteServiceDTO;
+import com.osgifx.console.data.provider.DataProvider;
+import com.osgifx.console.util.fx.DTOCellValueFactory;
+import com.osgifx.console.util.fx.Fx;
+
+import javafx.collections.transformation.FilteredList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.BorderPane;
+
+public final class RsaFxController {
+
+    @Log
+    @Inject
+    private FluentLogger                 logger;
+    @Inject
+    @LocalInstance
+    private FXMLLoader                   loader;
+    @FXML
+    private TableView<XRemoteServiceDTO> table;
+    @Inject
+    @OSGiBundle
+    private BundleContext                context;
+    @Inject
+    @Named("is_connected")
+    private boolean                      isConnected;
+    @Inject
+    private DataProvider                 dataProvider;
+    @Inject
+    private ThreadSynchronize            threadSync;
+
+    private FilteredList<XRemoteServiceDTO>         filteredList;
+    private TableRowDataFeatures<XRemoteServiceDTO> previouslyExpanded;
+    private boolean                                 isInitialized;
+
+    @FXML
+    public void initialize() {
+        if (!isConnected) {
+            Fx.addTablePlaceholderWhenDisconnected(table);
+            return;
+        }
+        if (!isCapabilityAvailable("RSA")) {
+            Fx.addTablePlaceholderWhenFeatureUnavailable(table, "Remote Service Admin");
+            return;
+        }
+        try {
+            if (!isInitialized) {
+                createControls();
+                Fx.disableSelectionModel(table);
+                isInitialized = true;
+            }
+            logger.atDebug().log("FXML controller has been initialized");
+        } catch (final Exception e) {
+            logger.atError().withException(e).log("FXML controller could not be initialized");
+        }
+    }
+
+    private boolean isCapabilityAvailable(final String capabilityId) {
+        return dataProvider.runtimeCapabilities().stream().anyMatch(c -> capabilityId.equals(c.id) && c.isAvailable);
+    }
+
+    private void createControls() {
+        final BorderPane                                expandedNode   = (BorderPane) Fx.loadFXML(loader, context,
+                "/fxml/expander-column-content.fxml");
+        final RsaDetailsFxController                    controller     = (RsaDetailsFxController) loader
+                .getController();
+        final TableRowExpanderColumn<XRemoteServiceDTO> expanderColumn = new TableRowExpanderColumn<>(current -> {
+                                                                           if (previouslyExpanded != null && current
+                                                                                   .getValue() == previouslyExpanded
+                                                                                           .getValue()) {
+                                                                               return expandedNode;
+                                                                           }
+                                                                           if (previouslyExpanded != null
+                                                                                   && previouslyExpanded.isExpanded()) {
+                                                                               previouslyExpanded.toggleExpanded();
+                                                                           }
+                                                                           controller.initControls(current.getValue());
+                                                                           previouslyExpanded = current;
+                                                                           return expandedNode;
+                                                                       });
+        expanderColumn.setPrefWidth(48);
+        expanderColumn.setMaxWidth(48);
+        expanderColumn.setMinWidth(48);
+
+        final TableColumn<XRemoteServiceDTO, String> idColumn = new TableColumn<>("ID");
+        idColumn.setCellValueFactory(new DTOCellValueFactory<>("id", String.class));
+
+        final TableColumn<XRemoteServiceDTO, String> directionColumn = new TableColumn<>("Direction");
+        directionColumn.setCellValueFactory(new DTOCellValueFactory<>("direction", String.class));
+
+        final TableColumn<XRemoteServiceDTO, String> providerColumn = new TableColumn<>("Provider");
+        providerColumn.setCellValueFactory(new DTOCellValueFactory<>("provider", String.class));
+
+        table.getColumns().add(expanderColumn);
+        table.getColumns().add(idColumn);
+        table.getColumns().add(directionColumn);
+        table.getColumns().add(providerColumn);
+        table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY_ALL_COLUMNS);
+
+        filteredList = new FilteredList<>(dataProvider.remoteServices());
+        threadSync.asyncExec(() -> {
+            table.setItems(filteredList);
+            TableFilter.forTableView(table).lazy(true).apply();
+            table.getSortOrder().add(idColumn);
+            table.sort();
+        });
+    }
+
+}
