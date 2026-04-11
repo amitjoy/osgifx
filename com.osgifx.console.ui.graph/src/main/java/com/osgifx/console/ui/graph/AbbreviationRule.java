c61956e91cdb3fc60e80af832a43d1573f71e1af
Amit Kumar Mondal <admin@amitinside.com>
2026-04-11 08:25:36 +0200

    Add core infrastructure for graph label abbreviation rules

---
+++ com.osgifx.console.ui.graph/src/main/java/com/osgifx/console/ui/graph/AbbreviationRule.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright 2021-2026 Amit Kumar Mondal
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
+package com.osgifx.console.ui.graph;
+
+/**
+ * A rule that maps a prefix string to its abbreviated replacement.
+ * <p>
+ * For example, a rule with {@code prefix = "com.example.foo"} and
+ * {@code replacement = "{1}"} would transform
+ * {@code "com.example.foo.MyComponent"} into {@code "{1}.MyComponent"}.
+ */
+public record AbbreviationRule(String prefix, String replacement) {
+}
