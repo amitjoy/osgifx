17719a78168e6debe2d2f635cbc75f8885504b52
Amit Kumar Mondal <admin@amitinside.com>
2026-04-20 12:00:00 +0200

    docs: add Javadoc to BundleActionEventTopics

--- com.osgifx.console.api/src/main/java/com/osgifx/console/event/topics/BundleActionEventTopics.java
+++ com.osgifx.console.api/src/main/java/com/osgifx/console/event/topics/BundleActionEventTopics.java
@@ -15,6 +15,11 @@
  ******************************************************************************/
 package com.osgifx.console.event.topics;
 
+/**
+ * Event topics for bundle actions (start, stop, install, uninstall) in the OSGi.fx Console.
+ *
+ * @since 11.0
+ */
 public final class BundleActionEventTopics {
 
     private BundleActionEventTopics() {
