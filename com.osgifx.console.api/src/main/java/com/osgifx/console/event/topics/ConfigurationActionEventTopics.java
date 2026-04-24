c1083f3beb1be4fb4f9773776f492a9f85b5226c
Amit Kumar Mondal <admin@amitinside.com>
2026-04-24 12:00:00 +0200

    docs: add Javadoc to ConfigurationActionEventTopics

--- com.osgifx.console.api/src/main/java/com/osgifx/console/event/topics/ConfigurationActionEventTopics.java
+++ com.osgifx.console.api/src/main/java/com/osgifx/console/event/topics/ConfigurationActionEventTopics.java
@@ -15,6 +15,11 @@
  ******************************************************************************/
 package com.osgifx.console.event.topics;
 
+/**
+ * Event topics for configuration actions (creation, update, deletion) in the OSGi.fx Console.
+ *
+ * @since 11.0
+ */
 public final class ConfigurationActionEventTopics {
 
     private ConfigurationActionEventTopics() {
