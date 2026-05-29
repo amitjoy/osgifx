6aa530a0c3be694f9a4799dc2b6e590af96c1286
Amit Kumar Mondal <admin@amitinside.com>
2026-05-29 12:00:00 +0200

    docs: add Javadoc to PayloadMetadata

--- com.osgifx.console.agent.api/src/main/java/com/osgifx/console/agent/spi/payload/PayloadMetadata.java
+++ com.osgifx.console.agent.api/src/main/java/com/osgifx/console/agent/spi/payload/PayloadMetadata.java
@@ -20,6 +20,11 @@ package com.osgifx.console.agent.spi.payload;
  *
  * @since 11.0
  */
+/**
+ * Metadata associated with a large payload transaction.
+ *
+ * @since 11.0
+ */
 public final class PayloadMetadata {
 
     /**
